a88aaad3c2e851697323c9289b575400bfdae03a
Benjamin Davies <b.davies@mmu.ac.uk>
2023-09-07 21:17:29 +0100

    Added a press, awards, and layout design section to the about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -12,19 +12,47 @@
             </v-img>
           </v-col>
           <v-col>
-            <h1>About me</h1>
+            <h2>About me</h2>
             <p>I’m a developer and designer based in Manchester, UK, with nearly five years of experience at Manchester Metropolitan University. My work primarily involves developing resources focused on learning, teaching, and assessment. I hold a Master's degree from the University of Central Lancashire, which I earned in 2016. Over the years, I've had the opportunity to work across various media. Previously, I dabbled in building video game worlds and creating books. Currently, my efforts are centered around video production, web development, and e-learning. I have a strong passion for topics such as art, film, curation, and creativity.</p>
           </v-col>
       </v-row>
-      <v-row>
+      <hr class="mt-12 mb-12">
+      <v-row class="achievements">
           <v-col>
-            
-          </v-col>
-          <v-col>
+            <h2>Layout design</h2>
+            <v-list>
+                <v-list-item>
+                  Artist-Led Interviews: North, book, self-published, October – November 2017
+                </v-list-item>
+                <v-list-item>The Archive of Gwyneth Davis, book, published by Typ-ooo North, August – September 2017</v-list-item>
+                <v-list-item>Works for Air, catalogue, published by CRISAP, November 2016</v-list-item>
+                <v-list-item>Of Carbonization, artists book, self-published, August 2016</v-list-item>
+                <v-list-item>A Guide To Living and Working in the Museum, artists book, self-published, August 2016</v-list-item>
+                <v-list-item>Twenty Fourteen, catalogue, published by University of Central Lancashire, May 2014</v-list-item>
+            </v-list>
 
           </v-col>
-          <v-col>
 
+          <v-col>
+            <h2>Awards</h2>
+            <v-list>
+              <v-list-item>Manchester Met Staff Awards nomination for Service Excellence Award, 2023</v-list-item>
+              <v-list-item>Manchester Met Staff Awards nomination for University Citizenship Award, 2023</v-list-item>
+              <v-list-item>Staff Bonus Scheme, 2023</v-list-item>
+              <v-list-item>Think Big Level 2 (funding), 2016</v-list-item>
+              <v-list-item>The Awesome Foundation (funding), 2015</v-list-item>
+              <v-list-item>Think Big Level 1 (funding), 2015</v-list-item>
+              <v-list-item>Creative Focus Award nomination, 2014</v-list-item>
+            </v-list>
+          </v-col>
+
+          <v-col>
+              <h2>Press</h2>
+              <v-list>
+                <v-list-item>Artist-led Hot 100 (A Small View), Published by a-n, words by Kevin Hunt, 2017 - <a href="https://www.a- n.co.uk/news/new-artist-led-hot-100-launched-assembly-liverpool/">https://www.a- n.co.uk/news/new-artist-led-hot-100-launched-assembly-liverpool/</a></v-list-item>
+                <v-list-item>Half-Real (review), published by The Skinny, words by Santini Basra, 2016 - <a href="http://www.theskinny.co.uk/art/reviews/half-real-hanover-project-preston">www.theskinny.co.uk/art/reviews/half-real-hanover-project-preston</a></v-list-item>
+                <v-list-item>The Holodeck (review), published by Art in Liverpool, Word by Kyle Nathan Brown, 2015 – <a href="http:// www.artinliverpool.com/?p=57712">http:// www.artinliverpool.com/?p=57712</a></v-list-item>
+              </v-list>
           </v-col>
       </v-row>
   </v-container>
