abe93054222c30568e285e8a5b423564ced3e365
Benjamin Davies <b.davies@mmu.ac.uk>
2023-09-10 20:43:31 +0100

    Added mobile breakpoints to skills and tech section in the description

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -2,14 +2,16 @@
     <div class="mb-12">
       <h2>{{ d.title }}</h2>
       <h3 v-if="d.start"><span>{{ d.start }} - </span>{{ d.end }}, {{ d.location }}</h3>
-      <v-row>
-        <v-col cols="12" md="6">
-          <h3 class="mt-6">Description</h3>
-          <div v-html="d.description"></div>
+      <v-row class="skills">
+        <v-col class="description" cols="12" md="6">
+          <div class="mr-12 mt-6">
+            <h3>Description</h3>
+            <div v-html="d.description"></div>
+          </div>
         </v-col>
             <v-col :cols="12" sm="12" md="6">
-              <div class="d-flex skills">
-                <div class="mt-6 mr-6"  v-for="(list, title) in { 'Technology': d.techUsed }" :key="title" >
+              <div class="d-flex">
+                <div class="mt-6 ml-auto mr-12"  v-for="(list, title) in { 'Technology': d.techUsed }" :key="title" >
                   <h3>{{ title }}</h3>
                   <v-list density="compact" lines="0">
                     <v-list-item
@@ -21,7 +23,7 @@
                     ></v-list-item>
                   </v-list>
                 </div>
-                <div class="mt-6"  v-for="(list, title) in { 'Skills': d.skills }" :key="title" >
+                <div class="mt-6" v-for="(list, title) in { 'Skills': d.skills }" :key="title" >
                   <h3>{{ title }}</h3>
                   <v-list density="compact" lines="0">
                     <v-list-item
